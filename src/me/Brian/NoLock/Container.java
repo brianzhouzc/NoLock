2a52535fa96a6836519475d03f00cad6808e43cf
Brian <Brian1461748123@gmail.com>
2015-03-01 11:32:58 -0800

    更新
    增加Container类，增加org json api，移除gson

---
+++ src/me/Brian/NoLock/Container.java
@@ -0,0 +1,161 @@
+package me.Brian.NoLock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.server.v1_8_R1.INamableTileEntity;
+import net.minecraft.server.v1_8_R1.TileEntity;
+import net.minecraft.server.v1_8_R1.TileEntityBrewingStand;
+import net.minecraft.server.v1_8_R1.TileEntityChest;
+import net.minecraft.server.v1_8_R1.TileEntityDispenser;
+import net.minecraft.server.v1_8_R1.TileEntityDropper;
+import net.minecraft.server.v1_8_R1.TileEntityEnchantTable;
+import net.minecraft.server.v1_8_R1.TileEntityFurnace;
+import net.minecraft.server.v1_8_R1.TileEntityHopper;
+
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+public class Container {
+	String rawdata;
+	String owner;
+	List<String> users = new ArrayList<String>();
+
+	public Container(Block block) {
+		this.rawdata = getRawData(block);
+		JSONObject jsonobj = new JSONObject(this.rawdata);
+		this.owner = jsonobj.getString("Owner");
+		JSONArray jsonarray = new JSONArray(jsonobj.get("Users").toString());
+		for (int i = 0; i < jsonarray.length(); i++) {
+			this.users.add(jsonarray.get(i).toString());
+		}
+	}
+
+	public Container(String rawdata) {
+		this.rawdata = rawdata;
+		JSONObject jsonobj = new JSONObject(this.rawdata);
+		this.owner = jsonobj.getString("Owner");
+		JSONArray jsonarray = new JSONArray(jsonobj.get("Users").toString());
+		for (int i = 0; i < jsonarray.length(); i++) {
+			this.users.add(jsonarray.get(i).toString());
+		}
+	}
+
+	public String getRawData() {
+		return this.rawdata;
+	}
+
+	public static String getRawData(Block block) {
+		final CraftWorld world = (CraftWorld) block.getWorld();
+		final TileEntity nmsTileEntity = world.getTileEntityAt(block.getX(), block.getY(), block.getZ());
+
+		if (nmsTileEntity instanceof INamableTileEntity) {
+			return ((INamableTileEntity) nmsTileEntity).getName();
+		}
+		return null;
+	}
+
+	public String getOwner() {
+		return this.owner;
+	}
+
+	public static String getOwner(String rawdata) {
+		JSONObject jsonobj = new JSONObject(rawdata);
+		return jsonobj.getString("Owner");
+	}
+
+	public List getUsers() {
+		return this.users;
+	}
+
+	public static List getUsers(String rawdata) {
+		JSONObject jsonobj = new JSONObject(rawdata);
+		if (jsonobj.has("Users")) {
+			JSONArray jsonarray = new JSONArray(jsonobj.get("Users").toString());
+			List<String> users = new ArrayList<String>();
+			for (int i = 0; i < jsonarray.length(); i++) {
+				users.add(jsonarray.get(i).toString());
+			}
+			return users;
+		}
+		return null;
+	}
+
+	public static boolean setRawData(Block block, String rawdata) {
+		final CraftWorld world = (CraftWorld) block.getWorld();
+		final TileEntity nmsTileEntity = world.getTileEntityAt(block.getX(), block.getY(), block.getZ());
+
+		if (nmsTileEntity instanceof INamableTileEntity) {
+			if (nmsTileEntity instanceof TileEntityChest) {
+				((TileEntityChest) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityFurnace) {
+				((TileEntityFurnace) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityDispenser) {
+				((TileEntityDispenser) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityDropper) {
+				((TileEntityDropper) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityHopper) {
+				((TileEntityHopper) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityBrewingStand) {
+				((TileEntityBrewingStand) nmsTileEntity).a(rawdata);
+			} else if (nmsTileEntity instanceof TileEntityEnchantTable) {
+				((TileEntityEnchantTable) nmsTileEntity).a(rawdata);
+			}
+			nmsTileEntity.update();
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean setRawData(Block block, String owner, List users, Object extradata) {
+		if (block != null && owner != null) {
+			JSONStringer stringer = new JSONStringer();
+			stringer.object().key("Owner").value(owner);
+			if (users != null) {
+				stringer.key("Users").value(users);
+			}
+			if (extradata != null) {
+				stringer.object().key("Extradata").value(extradata);
+			}
+			setRawData(block, stringer.endObject().toString());
+			return true;
+			// System.out.println(stringer.endObject().toString());
+		}
+		return false;
+	}
+
+	public static boolean addUsers(Block block, String rawdata, List users) {
+		try {
+			JSONObject jsonobj = new JSONObject(rawdata);
+			if (getUsers(rawdata) != null) {
+				jsonobj.put("Users", users.add(jsonobj.get("Users")));
+			} else {
+				jsonobj.put("Users", users);
+			}
+			return setRawData(block, jsonobj.toString());
+			// System.out.println(jsonobj.toString());
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static boolean addUsers(Block block, List users) {
+		if (getRawData(block) != null) {
+			try {
+				JSONObject jsonobj = new JSONObject(getRawData(block));
+				if (getUsers(getRawData(block)) != null) {
+					jsonobj.put("Users", users.add(jsonobj.get("Users")));
+				} else {
+					jsonobj.put("Users", users);
+				}
+				return setRawData(block, jsonobj.toString());
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		return false;
+	}
+}
--- src/me/Brian/NoLock/Main.java
+++ src/me/Brian/NoLock/Main.java
@@ -1,7 +1,7 @@
 package me.Brian.NoLock;
 
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.minecraft.server.v1_8_R1.INamableTileEntity;
 import net.minecraft.server.v1_8_R1.TileEntity;
@@ -14,27 +14,47 @@ import net.minecraft.server.v1_8_R1.TileEntityFurnace;
 import net.minecraft.server.v1_8_R1.TileEntityHopper;
 
 import org.bukkit.block.Block;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	static Plugin plugin;
 
+	public static void main(String[] args) {
+		Container container = new Container(
+				"{\"Owner\":\"97f10862-7eef-4755-979e-238253cf4677\",\"Users\":[\"69abcfbf-991d-42a3-8c1d-10787eae7949\",\"9e550853-9826-40d4-b5d5-29f5653aaf0e\",\"fa3c1f7a-f18b-4629-b077-4e7a2c333f04\"]}");
+		// System.out.println(container.getRawData());
+		// System.out.println(container.getOwner());
+		// System.out.println(container.getUsers());
+		List users = new ArrayList<>();
+		users.add("test");
+		users.add("test2");
+		Container.addUsers(null, "{\"Owner\":\"97f10862-7eef-4755-979e-238253cf4677\"}", users);
+	}
+
 	public void onEnable() {
 		plugin = this;
 		this.getServer().getPluginManager().registerEvents(this, this);
+		// Gson gson = new Gson();
+		// User user = gson.fromJson("{\"name\":\"test\",\"UUID\":\"test1\"}", User.class);
+		// System.out.println(user);
 	}
 
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if (event.getAction() == Action.LEFT_CLICK_BLOCK && event.getPlayer().isSneaking()) {
-			setName("Test", event.getClickedBlock());
-			event.getPlayer().sendMessage("success!");
-			event.setCancelled(true);
+			if (Container.addUsers(event.getClickedBlock(), event.getPlayer().getName(), null)) {
+				event.getPlayer().sendMessage("success!");
+				event.setCancelled(true);
+			}
+			// Container.setRawData(event.getClickedBlock(), event.getPlayer().getUniqueId().toString(), null, null);
+			// event.getPlayer().sendMessage("success!");
+			// event.setCancelled(true);
 		} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getPlayer().isSneaking()) {
 			event.getPlayer().sendMessage(getName(event.getClickedBlock()));
 			event.setCancelled(true);
