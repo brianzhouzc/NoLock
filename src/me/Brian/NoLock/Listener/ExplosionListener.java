fb7df299d3ecebfcdebcc37747771045daa8f45f
Brian <Brian1461748123@gmail.com>
2015-03-02 22:45:36 -0800

    Perfect ExplosionListener

--- src/me/Brian/NoLock/Listener/ExplosionListener.java
+++ src/me/Brian/NoLock/Listener/ExplosionListener.java
@@ -1,6 +1,9 @@
 package me.Brian.NoLock.Listener;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import me.Brian.NoLock.API.Container;
 
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
@@ -13,8 +16,12 @@ public class ExplosionListener implements Listener {
 	@EventHandler
 	public void onExplode(EntityExplodeEvent event) {
 		List<Block> list = event.blockList();
+		List<Block> deniedblocklist = new ArrayList<Block>();
 		for (int i = 0; i < list.size(); i++) {
-
+			Block block = list.get(i);
+			if (Container.isContainer(block)) {
+				deniedblocklist.add(block);
+			}
 		}
 	}
 }
