9f5574ea49d2240216e298bbd1dc017bb835e704
connection-lost <frankwu1995@hotmail.com>
2015-03-05 11:03:19 -0500

    Surpressed warning on jsonlib

--- src/org/json/JSONObject.java
+++ src/org/json/JSONObject.java
@@ -298,7 +298,8 @@ public class JSONObject {
      */
     public JSONObject(Object object, String names[]) {
         this();
-        Class c = object.getClass();
+        @SuppressWarnings("rawtypes")
+		Class c = object.getClass();
         for (int i = 0; i < names.length; i += 1) {
             String name = names[i];
             try {
@@ -631,7 +632,8 @@ public class JSONObject {
         if (object == null) {
             return null;
         }
-        Class klass = object.getClass();
+        @SuppressWarnings("rawtypes")
+		Class klass = object.getClass();
         Field[] fields = klass.getFields();
         int length = fields.length;
         if (length == 0) {
@@ -981,7 +983,8 @@ public class JSONObject {
     }
 
     private void populateMap(Object bean) {
-        Class klass = bean.getClass();
+        @SuppressWarnings("rawtypes")
+		Class klass = bean.getClass();
 
 // If klass is a System class then set includeSuperClass to false.
 
@@ -1488,7 +1491,8 @@ public class JSONObject {
      * @throws JSONException
      *             If the value is or contains an invalid number.
      */
-    public static String valueToString(Object value) throws JSONException {
+    @SuppressWarnings("unchecked")
+	public static String valueToString(Object value) throws JSONException {
         if (value == null || value.equals(null)) {
             return "null";
         }
@@ -1535,7 +1539,8 @@ public class JSONObject {
      *            The object to wrap
      * @return The wrapped value
      */
-    public static Object wrap(Object object) {
+    @SuppressWarnings("unchecked")
+	public static Object wrap(Object object) {
         try {
             if (object == null) {
                 return NULL;
@@ -1586,7 +1591,8 @@ public class JSONObject {
         return this.write(writer, 0, 0);
     }
 
-    static final Writer writeValue(Writer writer, Object value,
+    @SuppressWarnings("unchecked")
+	static final Writer writeValue(Writer writer, Object value,
             int indentFactor, int indent) throws JSONException, IOException {
         if (value == null || value.equals(null)) {
             writer.write("null");
