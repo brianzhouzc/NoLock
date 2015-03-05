8b94fa7c43e23953a733ab8fb44eba0853a7d696
Brian <Brian1461748123@gmail.com>
2015-03-05 08:12:44 -0800

    Add more options to ProtocolLib name overide

--- src/me/Brian/NoLock/Main.java
+++ src/me/Brian/NoLock/Main.java
@@ -1,5 +1,7 @@
 package me.Brian.NoLock;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -13,6 +15,7 @@ import me.Brian.NoLock.Listener.ExplosionListener;
 import me.Brian.NoLock.Wrapper.WrapperPlayServerOpenWindow;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -32,7 +35,34 @@ public class Main extends JavaPlugin implements Listener {
 	static Logger logger = null;
 
 	public static void main(String[] args) {
-		System.out.println("{\"Owner\":\"" + "fa3c1f7a-f18b-4629-b077-4e7a2c333f04" + "\",\"Users\":[\"9e550853-9826-40d4-b5d5-29f5653aaf0e\",\"fa3c1f7a-f18b-4629-b077-4e7a2c333f04\"]}");
+		List users = new ArrayList<>();
+		String titile;
+		String owner = ChatColor.RED + "030";
+		users.add("Brian14617481234567890");
+		users.add("Raider_Soap");
+		users.add("Zergpvz_141");
+		titile = owner;
+		if (users != null) {
+			titile = titile + ChatColor.RESET;
+			for (int i = 0; i < users.size(); i++) {
+				if (titile.length() + users.get(i).toString().length() <= 31) {
+					titile = titile + ", " + users.get(i);
+				} else {
+					if (titile.length() <= 28) {
+						titile = titile + "...";
+						System.out.println("1");
+					} else {
+						titile = titile.substring(0, 28) + "...";
+						System.out.println("2");
+					}
+					i = users.size();
+				}
+
+			}
+			System.out.println(titile);
+			System.out.println(titile.length());
+		}
+		System.out.println("¡ìr".length());
 	}
 
 	public void onEnable() {
@@ -45,69 +75,91 @@ public class Main extends JavaPlugin implements Listener {
 		pm.registerEvents(new BlockBreakListener(), this);
 		getCommand("nolock").setExecutor(new CommandListener());
 		saveDefaultConfig();
-		logger.info(Config.EnableProtocollibNameOveride() + "");
-		logger.info(Config.EnableProtocollibNameOverideUsersName() + "");
-		// logger.info(org.bukkit.Bukkit.getOfflinePlayer(UUID.fromString("97f10862-7eef-4755-979e-238253cf4677")).getName());
-		logger.info("[NoLock] NoLock " + getDescription().getVersion() + " successfuly loaded!");
 
 		if (Config.EnableProtocollibNameOveride()) {
-			ProtocolLibrary.getProtocolManager().addPacketListener(new PacketListener() {
-				public Plugin getPlugin() {
-					return Bukkit.getPluginManager().getPlugin("NoLock");
-				}
+			if (getPluginManager().getPlugin("ProtocolLib") != null) {
+				ProtocolLibrary.getProtocolManager().addPacketListener(new PacketListener() {
+					public Plugin getPlugin() {
+						return Bukkit.getPluginManager().getPlugin("NoLock");
+					}
 
-				public ListeningWhitelist getReceivingWhitelist() {
-					return ListeningWhitelist.newBuilder().gamePhase(GamePhase.PLAYING).highest().types(PacketType.Play.Server.OPEN_WINDOW).build();
-				}
+					public ListeningWhitelist getReceivingWhitelist() {
+						return ListeningWhitelist.newBuilder().gamePhase(GamePhase.PLAYING).highest().types(PacketType.Play.Server.OPEN_WINDOW).build();
+					}
 
-				public ListeningWhitelist getSendingWhitelist() {
-					return ListeningWhitelist.newBuilder().gamePhase(GamePhase.PLAYING).highest().types(PacketType.Play.Server.OPEN_WINDOW).build();
-				}
+					public ListeningWhitelist getSendingWhitelist() {
+						return ListeningWhitelist.newBuilder().gamePhase(GamePhase.PLAYING).highest().types(PacketType.Play.Server.OPEN_WINDOW).build();
+					}
 
-				public void onPacketReceiving(PacketEvent e) {
-					return;
-				}
-
-				@Override
-				public void onPacketSending(PacketEvent e) {
-					if (!(e.getPacket().getType() == PacketType.Play.Server.OPEN_WINDOW))
+					public void onPacketReceiving(PacketEvent e) {
 						return;
+					}
 
-					WrapperPlayServerOpenWindow wp = new WrapperPlayServerOpenWindow(e.getPacket());
-					String rawdata = wp.getWindowTitle().getJson().toString().replaceAll("\\\\\"", "\"").replace("\"{", "{").replace("}\"", "}");
-					Bukkit.broadcastMessage(rawdata);
-					// Bukkit.broadcastMessage(wp.getInventoryType());
-					if (Container.isContainer(rawdata)) {
-						if (Container.getName(rawdata) != null) {
-							wp.setWindowTitle(WrappedChatComponent.fromJson("\"" + Container.getName(rawdata) + "\""));
-						} else {
-							String invtype = wp.getInventoryType();
-							String rawname = null;
+					@Override
+					public void onPacketSending(PacketEvent e) {
+						if (!(e.getPacket().getType() == PacketType.Play.Server.OPEN_WINDOW))
+							return;
 
-							if (Config.EnableProtocollibNameOverideUsersName()) {
-								rawname = "\"" + Bukkit.getOfflinePlayer(UUID.fromString(Container.getOwner(rawdata))).getName() + "\"";
+						WrapperPlayServerOpenWindow wp = new WrapperPlayServerOpenWindow(e.getPacket());
+						String rawdata = wp.getWindowTitle().getJson().toString().replaceAll("\\\\\"", "\"").replace("\"{", "{").replace("}\"", "}");
+						Bukkit.broadcastMessage(rawdata);
+						// Bukkit.broadcastMessage(wp.getInventoryType());
+						if (Container.isContainer(rawdata)) {
+							if (Container.getName(rawdata) != null) {
+								wp.setWindowTitle(WrappedChatComponent.fromJson("\"" + Container.getName(rawdata) + "\""));
 							} else {
-								if (invtype.equalsIgnoreCase("minecraft:chest")) {
-									rawname = "{\"translate\":\"container.chest\"}";
-								} else if (invtype.equalsIgnoreCase("minecraft:furnace")) {
-									rawname = "{\"translate\":\"container.furnace\"}";
-								} else if (invtype.equalsIgnoreCase("minecraft:dispenser")) {
-									rawname = "{\"translate\":\"container.dispenser\"}";
-								} else if (invtype.equalsIgnoreCase("minecraft:dropper")) {
-									rawname = "{\"translate\":\"container.dropper\"}";
-								} else if (invtype.equalsIgnoreCase("minecraft:brewing_stand")) {
-									rawname = "{\"translate\":\"container.brewing\"}";
-								} else if (invtype.equalsIgnoreCase("minecraft:enchanting_table")) {
-									rawname = "{\"translate\":\"container.enchant\"}";
+								String invtype = wp.getInventoryType();
+								String rawname = null;
+
+								if (Config.EnableProtocollibNameOverideUsersName()) {
+									String titile;
+									String owner = ChatColor.RED + Bukkit.getOfflinePlayer(UUID.fromString(Container.getOwner(rawdata))).getName();
+									List<String> users = Container.getUsers(rawdata);
+									titile = owner;
+									if (users != null) {
+										titile = titile + ChatColor.RESET;
+										for (int i = 0; i < users.size(); i++) {
+											if (titile.length() + Bukkit.getOfflinePlayer(UUID.fromString(users.get(i))).toString().length() <= 31) {
+												titile = titile + ", " + Bukkit.getOfflinePlayer(UUID.fromString(users.get(i)));
+											} else {
+												if (titile.length() <= 28) {
+													titile = titile + "...";
+												} else {
+													titile = titile.substring(0, 28) + "...";
+												}
+												i = users.size();
+											}
+										}
+									}
+									rawname = "\"" + titile + "\"";
 								} else {
-									rawname = "\"Container\"";
+									if (invtype.equalsIgnoreCase("minecraft:chest")) {
+										rawname = "{\"translate\":\"container.chest\"}";
+									} else if (invtype.equalsIgnoreCase("minecraft:furnace")) {
+										rawname = "{\"translate\":\"container.furnace\"}";
+									} else if (invtype.equalsIgnoreCase("minecraft:dispenser")) {
+										rawname = "{\"translate\":\"container.dispenser\"}";
+									} else if (invtype.equalsIgnoreCase("minecraft:dropper")) {
+										rawname = "{\"translate\":\"container.dropper\"}";
+									} else if (invtype.equalsIgnoreCase("minecraft:brewing_stand")) {
+										rawname = "{\"translate\":\"container.brewing\"}";
+									} else if (invtype.equalsIgnoreCase("minecraft:enchanting_table")) {
+										rawname = "{\"translate\":\"container.enchant\"}";
+									} else {
+										rawname = "\"Container\"";
+									}
 								}
+								wp.setWindowTitle(WrappedChatComponent.fromJson(rawname));
 							}
-							wp.setWindowTitle(WrappedChatComponent.fromJson(rawname));
 						}
 					}
-				}
-			});
+				});
+			} else {
+				logger.warning("[NoLock] Can't found ProtocolLib! Disabling plugin... Please install ProtocolLib or change settings in config file.");
+				getPluginManager().disablePlugins();
+				return;
+			}
+			logger.info("[NoLock] NoLock " + getDescription().getVersion() + " successfuly loaded!");
 		}
 	}
 
