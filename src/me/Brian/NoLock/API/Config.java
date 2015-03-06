cf54894900515944574558662bcee4ee298e3733
connection-lost <frankwu1995@hotmail.com>
2015-03-05 19:34:30 -0500

    Added default action in config reading

--- src/me/Brian/NoLock/API/Config.java
+++ src/me/Brian/NoLock/API/Config.java
@@ -9,34 +9,34 @@ public class Config {
 	static Plugin plugin = Main.getPlugin();
 
 	public static boolean ExplotionProtectionAll() {
-		return plugin.getConfig().getBoolean("Configs.explosion-protection-all");
+		return plugin.getConfig().getBoolean("Configs.explosion-protection-all", true);
 	}
 
 	public static boolean AdminSnoop() {
-		return plugin.getConfig().getBoolean("Configs.allow-admin-snoop");
+		return plugin.getConfig().getBoolean("Configs.allow-admin-snoop", true);
 	}
 
 	public static boolean AdminBreak() {
-		return plugin.getConfig().getBoolean("Configs.allow-admin-break");
+		return plugin.getConfig().getBoolean("Configs.allow-admin-break", true);
 	}
 
 	public static boolean EnableQuickProtection() {
-		return plugin.getConfig().getBoolean("Configs.quick-protect.enable");
+		return plugin.getConfig().getBoolean("Configs.quick-protect.enable", false);
 	}
 
 	public static Material getQuickProtectMaterial() {
-		return Material.getMaterial(plugin.getConfig().getString("Configs.quick-protect.item"));
+		return Material.getMaterial(plugin.getConfig().getString("Configs.quick-protect.item", "STICK"));
 	}
 
 	public static boolean EnableProtocollibNameOveride() {
-		return plugin.getConfig().getBoolean("Configs.protocollib-name-overide.enable");
+		return plugin.getConfig().getBoolean("Configs.protocollib-name-overide.enable", true);
 	}
 	
 	public static boolean EnableProtocollibNameOverideUsersName() {
-		return plugin.getConfig().getBoolean("Configs.protocollib-name-overide.use_users_name");
+		return plugin.getConfig().getBoolean("Configs.protocollib-name-overide.use_users_name", true);
 	}
 
 	public static boolean HopperBlocking() {
-		return plugin.getConfig().getBoolean("Configs.enable-hopper-blocking");
+		return plugin.getConfig().getBoolean("Configs.enable-hopper-blocking", true);
 	}
 }
